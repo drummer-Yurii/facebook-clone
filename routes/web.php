cfd8aba81c51c15eed533f0791fd72d2dcdc0969
Yurii Rybachok <drummer2009@ukr.net>
2023-04-09 10:46:57 +0300

    laravel router endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -16,15 +17,13 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Posts');
-});
-
 Route::get('/user', function () {
     return Inertia::render('User');
 });
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', [PostController::class, 'index'])->name('posts.index');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
